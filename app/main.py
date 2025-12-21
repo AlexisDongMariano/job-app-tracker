c52d352cbd3ee662086d9e4ac5bbb8bb745d1ab5
Alexis Mariano <alexisdongmariano@gmail.com>
2025-12-21 07:51:28 -0800

    add app form and in-memory storage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -9,15 +9,34 @@ app.mount('/static', StaticFiles(directory='app/static'), name='static')
 
 templates = Jinja2Templates(directory='app/templates')
 
-@app.get('/', response_class=HTMLResponse)
-def index(request: Request):
-    job_applications = [{
+job_applications = [{
         'company': 'company1',
         'role': 'role1',
         'status': 'job offer'
     }]
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
     return templates.TemplateResponse(
         'index.html',
-        {'request': request, 'job_applications': job_applications},
+        {
+            'request': request, 
+            'job_applications': job_applications
+        },
     )
+
+@app.post('/applications')
+def add_application(
+    company: str = Form(...),
+    role: str = Form(...),
+    status: str = Form(...),
+):
+    job_applications.append(
+        {
+            'company': company,
+            'role': role,
+            'status': status,
+        }
+    )
+    return RedirectResponse(url='/', status_code=303)
     
