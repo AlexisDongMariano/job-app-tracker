a6f6f163666becfeb03ab487838c4548b70b0ab9
Alexis Mariano <alexisdongmariano@gmail.com>
2025-12-21 07:40:25 -0800

    render HTML page with templates

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return {'status': 'ok', 'app': 'job-tracker'}
+app.mount('/static', StaticFiles(directory='app/static'), name='static')
+
+templates = Jinja2Templates(directory='app/templates')
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    job_applications = [{
+        'company': 'company1',
+        'role': 'role1',
+        'status': 'job offer'
+    }]
+    return templates.TemplateResponse(
+        'index.html',
+        {'request': request, 'job_applications': job_applications},
+    )
+    
