969e95ec12c0eb81582ba29ff3c7f67886c92125
Alexis Mariano <alexisdongmariano@gmail.com>
2026-01-23 23:55:42 -0800

    added starting terraform files

---
+++ infra/terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_version = ">= 1.0"
+
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 3.0"
+        }
+    }
+}
+
+# Optional: Store state remotely
+backend "azurerm" {
+    resource_group_name = "tfstate-rg"
+    storage_account_name = "tfstatestorage"
+    container_name = "tfstate"
+    key = "test.terraform.tfstate"
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "resource_group_name" {
+    type = string
+    default = "adm-rg" 
+}
+
+variable "location" {
+    description = "Azure region"
+    type = string
+    default = "canadacentral"
+}
+
+variable "vm_size" {
+    type = string
+    default = "Standard_B1s"
+}
+
+variable "admin_username" {
+    type = string
+    default = "localadmin"
+}
+
+variable "vm_name" {
+    type = string
+    default = "adm-1-vm"
+}
